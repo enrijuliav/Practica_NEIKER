ea3e4012b2628b261cc2407b38a07b87ebd8d545
enrijuliav <enrijuliav@gmail.com>
2025-11-14 17:15:02 +0100

    Rcode

---
+++ Scripts/Fig3.R
@@ -0,0 +1,4 @@
+# Define a reusable function to load the data from the paper.
+# This data has the name of the figure before the colnames,
+# which complicates the loading process
+setwd("~/Documentos/GitHub/Practica_NEIKER")
